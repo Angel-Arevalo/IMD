9cba9eefca347111e93602e96e23e86c30b82591
Angel Arevalo <anarevalof@unal.edu.co>
2024-03-19 20:12:43 -0500

    Mejorando front-end

--- Back_end/DataBase/User.py
+++ Back_end/DataBase/User.py
@@ -3,16 +3,17 @@ import sqlite3 as sql
 Base_Direction = r'C:\Users\Angel Arevalo\OneDrive\Documentos\GitHub\Null\Back_end\DataBase\DataUsers.db'
 
 class InputUser:
-    def __init__(self, Usuario, Contraseña,Rol):
-        self.Usuario = Usuario
-        self.Contraseña = Contraseña
-        self.Rol = Rol
+    def __init__(self, Usuario, Contraseña,Rol, Email):#Uso str en cada declaración para no tener problemas con la base de datos
+        self.Usuario = str(Usuario)
+        self.Contraseña = str(Contraseña)
+        self.Rol = str(Rol)
+        self.Email = str(Email)
     
     def GuardarEnDataUsers(self):
         apuntador = sql.connect(Base_Direction)
         try:
-            Insert = "INSERT INTO Usuarios (Nombre_Usuario, Contraseña, Rol) VALUES (?, ?, ?)"
-            apuntador.execute(Insert, (self.Usuario, self.Contraseña, self.Rol))
+            insert = 'INSERT INTO Usuarios (Nombre_Usuario, Contraseña, Rol, Email) VALUES (?, ?, ?, ?)'
+            apuntador.execute(insert, (self.Usuario, self.Contraseña, self.Rol, self.Email))
             apuntador.commit()
         finally:
             apuntador.close()
