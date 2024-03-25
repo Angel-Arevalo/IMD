19bcd8d9610abdb193fecb627250ac8a4b40af66
Angel Arevalo <anarevalof@unal.edu.co>
2024-03-24 20:25:17 -0500

    Cambios

---
+++ front-end/Worlds/Tutorial/Tutotial.js
@@ -0,0 +1,24 @@
+var leer = localStorage.getItem('mensaje');
+
+addEventListener('load', function() {
+    if (leer == "1") {
+        localStorage.setItem('mensaje','0');
+    }else if(leer == "0") {
+        window.location.href = '../../Log-in/Login.html';
+    }  
+})
+
+function Trip(params) {
+    localStorage.setItem('mensaje','1');
+    if(params == "N1") {
+        window.location.href = "../Tutorial/Nivel1/Nivel1.html";
+    }if (params == "N2") {
+        window.location.href = "../Tutorial/Nivel2/Nivel2.html";
+    }if (params == "N3") {
+        window.location.href = "../Tutorial/Nivel3/Nivel3.html";
+    }
+}
+function Principal() {
+    localStorage.setItem('mensaje','1');
+    window.location.href = '../EscogerMundo.html';
+}
