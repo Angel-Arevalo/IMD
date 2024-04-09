dc3704ec9bb3c55c03a4d67ae501e88bab5afaf3
Angel Arevalo <anarevalof@unal.edu.co>
2024-04-09 15:33:10 -0500

    Terminando la evaluación 1

--- front-end/Worlds/MundoHistoria/Nivel1/Examen1.js
+++ front-end/Worlds/MundoHistoria/Nivel1/Examen1.js
@@ -1,5 +1,5 @@
-
-
+let Nota = 5, Veces = [false, false];
+let Respuesta = ['c', 'b', 'a', 'a', 'b']
 
 function Eleccion(ele) {
     if (ele == "Volver") {
@@ -8,5 +8,18 @@ function Eleccion(ele) {
     }else {
         document.getElementById('Instrucciones').style.display = "none";
         document.getElementById('Mostrar').style.display = "block";
+    }
+}
+
+function Mostrar(imagen) {
+    console.log(Nota);
+    if (imagen == "SistemaN" && !Veces[0]) {
+        document.getElementById('Sn').style.display = "block";
+        Nota -= 0.25;
+        Veces[0] = true;
+    }if (imagen == "sqrt" && !Veces[1]) {
+        Veces[1] = true;
+        Nota -= 0.25;
+        document.getElementById('sqrt').style.display = "block";
     }
 }
