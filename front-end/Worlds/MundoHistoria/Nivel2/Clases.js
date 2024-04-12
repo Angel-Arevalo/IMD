8fe5c774b68003328ef9e5d614781e24544cea43
Angel Arevalo <anarevalof@unal.edu.co>
2024-04-12 09:28:58 -0500

    Terminando el reto del Nivel 2

--- front-end/Worlds/MundoHistoria/Nivel2/Clases.js
+++ front-end/Worlds/MundoHistoria/Nivel2/Clases.js
@@ -1,4 +1,4 @@
-class MoverTriangulos {
+class MoverCuadrados {
     constructor(figura, referencia) {
         this.figura = figura;
         this.FiguraMoviendose = false;
@@ -6,12 +6,7 @@ class MoverTriangulos {
         this.InicioY = 0;
         this.Fijado = false;
         this.referencia = referencia;
-        //En transformaciones obtengo la matriz de giro de cada div
-        this.Transformaciones = this.ObtenerMatrizDeRotacion(referencia);
-        this.ModificadoRefX = false;
-        this.ModificadoRefY = false;
-        this.ModificarEscala = 0;
-        this.Grado = 0;
+        this.Rotacion = false;
         //Eventos
         this.figura.addEventListener('mousedown', this.iniciarArrastre.bind(this));
         this.figura.addEventListener('mousemove', this.arrastrar.bind(this));
@@ -39,44 +34,26 @@ class MoverTriangulos {
         this.FiguraMoviendose = false;
     }
 
-    CompararRotacion(objeto, transform) {
-        let Rotacion = window.getComputedStyle(objeto).getPropertyValue('transform');
-        let Igual = true;
-        
-        if (Rotacion == "none") {
-            Rotacion = ['1',' 0',' 0',' 1',' 0',' 0'];
-        }else {
-            Rotacion = Rotacion.substring(7, Rotacion.length - 1).split(',')
-        }
-
-        for (var i = 0; i < Rotacion.length; i++) {
-            if (Rotacion[i] != transform[i]) {
-                Igual = false;
-            }
-        }
-        return Igual;
-    }
-
     fijar() {
         //Declaración de variables
         let AyudaFijar = this.figura.getBoundingClientRect();
-        let Fijar, Rotacion;
+        let Fijar;
 
         for (let i = 0; i < this.referencia.length; i++) {
             Fijar = this.referencia[i].getBoundingClientRect();
-            Rotacion = this.CompararRotacion(this.figura, this.Transformaciones[i]);
             
              // Condición
             if (AyudaFijar.left - 25 <= Fijar.left &&
                 AyudaFijar.right + 25 >= Fijar.right &&
                 AyudaFijar.top - 25 <= Fijar.top &&
                 AyudaFijar.bottom + 25 >= Fijar.bottom 
-                && Rotacion ) {
+                && this.Rotacion && !posicionesCuadrados[i]) {
                     this.figura.style.left = Fijar.left + 'px';
                     this.figura.style.top = Fijar.top + 'px'; 
                     this.figura.style.width = Fijar.width + 'px';
                     this.figura.style.height = Fijar.height + 'px';
                     this.Fijado = true;
+                    posicionesCuadrados[i] = true;
                     let background = window.getComputedStyle(this.referencia[i]).getPropertyValue('background');
                     this.CrearDiv(background.substring(0, background.indexOf(')') + 1), this.referencia[i]);
                     this.soltar();
@@ -85,40 +62,9 @@ class MoverTriangulos {
         }  
     }
 
-    ModificarReflexion(sentido) {
-        if (sentido == 'x') {
-             if (!this.ModificadoRefX) {
-                this.figura.style.transform = "scaleX(-1)";
-                this.ModificadoRefX = true;
-            }else {
-                this.figura.style.transform = "scaleX(1)";
-                this.ModificadoRefX = false;
-            }
-        }else {
-            if (!this.ModificadoRefY) {
-                this.figura.style.transform = "scaleY(-1)";
-                this.ModificadoRefY = true;
-            }else {
-                this.figura.style.transform = "scaleY(1)";
-                this.ModificadoRefY = false;
-            }
-        }
-    }
-
     ModificarRotacion() {
-        this.Grado += 180;
-        this.figura.style.transform = `rotate(${this.Grado}deg)`;
-    }
-
-    ObtenerMatrizDeRotacion(ObjetoPorRevisar) {
-        let Lista = [];
-        for (var i = 0; i < ObjetoPorRevisar.length; i++) {
-            let Ayuda = window.getComputedStyle(ObjetoPorRevisar[i]).getPropertyValue('transform');
-            if (Ayuda == "none") {
-                Lista.push(['1',' 0',' 0',' 1',' 0',' 0']);
-            }else Lista.push(Ayuda.substring(7, Ayuda.length - 1).split(','));
-        }
-        return Lista;
+        this.figura.style.transform = "matrix(0.6, 0.8, -0.8, 0.6, 0, 0)";
+        this.Rotacion = true
     }
 
     CrearDiv(background, referencia) {
--- front-end/Worlds/MundoHistoria/Nivel2/Nivel.js
+++ front-end/Worlds/MundoHistoria/Nivel2/Nivel.js
@@ -1,1 +1,39 @@
+let jugando = false, Mover;
+let posicionesCuadrados = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+                           false, false, false, false, false, false, false, false, false, false]
 
+
+console.log(document.getElementsByClassName("CuadradoPequeño1"));
+
+function Jugar(nombre) {
+    if (!jugando) {
+        CrearObjeto(nombre);
+        Mover = new MoverCuadrados(document.getElementById('Mover'), document.getElementsByClassName("CuadradoPequeño1"));
+        jugando = true;
+    }else alert("Ya está moviendo otro cuadrado");
+}
+
+
+function CrearObjeto(objeto) {
+
+    let div = document.createElement('div');
+    let divPadre = document.getElementById(objeto);
+    let propiedades = divPadre.getBoundingClientRect();
+    console.log(divPadre.getBoundingClientRect());
+
+    div = divPadre.cloneNode(true);
+    div.id = 'Mover';
+
+    divPadre.style.background = "rgb(127, 127, 164)";
+    div.style.background = "rgb(230, 230, 134)";
+    div.style.border = "none";
+    div.style.position = "absolute";
+    div.style.top = (propiedades.top) + "px";
+    div.style.left = propiedades.left + "px";
+    div.style.right = propiedades.right + "px";
+    div.style.bottom = propiedades.bottom + "px";
+    div.removeAttribute('onclick');
+    console.log(div);
+
+    document.body.appendChild(div);
+}
