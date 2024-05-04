883e68ba33cd38c7c21c13dcd096e929e325545d
ArielDcg <cadenasantisteban@gmail.com>
2024-05-04 12:18:58 -0500

    Update Calificacion.py

--- src/Back_end/Models/Calificacion.py
+++ src/Back_end/Models/Calificacion.py
@@ -33,17 +33,49 @@ class Calificacion:
                 '''
         Cursor.Execute(sql_cmd)
 
-    def C_EntregarNotas(resultado, tipo):
-        match tipo:
-            case 1:
-                pass
-            case 2:
-                pass
-            case 3:
-                pass
-            case 4:
-                pass
-        pass
+    def toList (lista, i = 0):
+        l = len(lista)
+        if i < l and type(lista[i]) == tuple:
+            lista[i] = list(lista[i])
+            return Calificacion.toList(lista ,i+1)
+        return lista
+
+    def cleanList (lista, i = 0, j = 0):
+        li, lj = len(lista), len(lista[0])
+        if i >= li:
+            return lista
+        if i < li and j < lj:
+            k = True if (j+1 != lj) else False
+            if k and type(lista[i][j+1]) == str:
+                lista[i].pop(j)
+            return Calificacion.cleanList(lista, i, j+1)
+        if j >= lj:
+            return Calificacion.cleanList(lista, i = i+1, j = 0)
+
+    def separeList1(lista: list, i, newL: list = [], j = 0):
+        if (j >= len(lista[i])) or (j != 0 and type(lista[i][j]) == str):
+            del lista[i][:j]
+            return lista, newL
+        newL.append(lista[i][j])
+        return Calificacion.separeList1(lista, i, newL, j+1)
+
+    def separeList2(lista: list):
+        newL: list = []
+        for i in range(len(lista)):
+            j = 0
+            newL.append([])
+            copy = lista
+            while len(copy[i]) > 0:
+                copy, a = Calificacion.separeList1(copy, i, newL=[])
+                newL[i].append(a)
+                del copy[i][i:j]
+        return newL
+
+    def SepareList(lista):
+        lista = Calificacion.toList(lista)
+        lista = Calificacion.cleanList(lista)
+        lista = Calificacion.separeList2(lista)
+        return lista
 
     ## A esta funcion se la llama asi: Calificacion.NotasEstudiante('code') #Necesita almenos el codigo
     def NotasEstudiante(codigo, estudiante = "Aula", mundo= "Todos"): #Devuelve una lista de tuplas [(Estudiante_1), (Estudiante_2),...,(Estudiante_n)]
@@ -56,14 +88,12 @@ class Calificacion:
                         INNER JOIN 'Mundo4_{codigo}' AS M4 ON M3.Nombre_Estudiante = M4.Nombre_Estudiante
                     '''
             resultado = Cursor.FetchA(sql_cmd)
-            identificador = 1
         elif (estudiante == "Aula" and mundo != "Todos"): #Aqui hay un posible riesgo a que de error si el mundo no existe
             sql_cmd = f'''
                         SELECT M1.*
                         FROM 'Mundo{mundo}_{codigo}' AS M1
                     '''
             resultado = Cursor.FetchA(sql_cmd)
-            identificador = 2
         elif (estudiante != "Aula" and mundo == "Todos"): #Aqui hay un posible riesgo a que de error si se escribe mal el nombre del estudiante o no existe
             sql_cmd = f'''
                         SELECT M1.*, M2.*, M3.*, M4.*
@@ -74,7 +104,6 @@ class Calificacion:
                         WHERE M1.Nombre_Estudiante = '{estudiante}'
                     '''
             resultado = Cursor.FetchA(sql_cmd)
-            identificador = 3
         else: #Aqui hay un posible riesgo a que de error
             sql_cmd = f'''
                         SELECT M1.*
@@ -82,50 +111,8 @@ class Calificacion:
                         WHERE M1.Nombre_Estudiante = '{estudiante}'
                     '''
             resultado = Cursor.FetchA(sql_cmd)
-            identificador = 4
+        resultado = Calificacion.SepareList(resultado)
         return resultado
 
 a = Calificacion.NotasEstudiante("maw-710")
-
-def toList (lista, i = 0):
-    l = len(lista)
-    if i < l and type(lista[i]) == tuple:
-        lista[i] = list(lista[i])
-        return toList(lista ,i+1)
-    return lista
-
-def cleanList (lista, i = 0, j = 0):
-    li = len(lista)
-    lj = len(lista[0])
-    if i < li and j < lj:
-        if type(lista[i][j]) != str and lista[i][j] > 0:
-            lista[i].pop(j)
-        return cleanList(lista, 0, j+1)
-    if j == lj:
-        return cleanList(lista, i+1, j+1)
-    return lista
-
-def separeList1(lista: list, i, newL: list = [], j = 0):
-    if j != 0 and type(lista[i][j]) == str:
-        return newL
-    newL.append(lista[i][j])
-    return separeList1(lista, i, newL, j+1)
-
-def newList(lista):
-    newL = []
-    for i in range(len(lista)):
-        j = len(lista[i])
-        newL.append([])
-        newL[i] = separeList1(lista, i)
-        k = len(newL[i])
-        k = k // j
-        for m in range(k-1):
-            pass
-
-
-
-
-
-a = toList(a)
-a = cleanList(a)
-print(separeList1(a, 0))
+print(a)
