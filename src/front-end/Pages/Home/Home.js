49c6cc7ca2ad4b299bc6ac021ec7bdf441fbec0f
ArielDcg <cadenasantisteban@gmail.com>
2024-06-06 17:53:02 -0500

    Creacion del HomePage

---
+++ src/front-end/Pages/Home/Home.js
@@ -0,0 +1,9 @@
+var homeLinks = document.getElementById("homeLinks");
+
+    function showMenu(){
+        homeLinks.style.right = "0";
+    }
+
+    function hideMenu(){
+        homeLinks.style.right = "-300px";
+    }
