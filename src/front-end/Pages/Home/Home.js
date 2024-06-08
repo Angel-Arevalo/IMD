d6c26de4ba778aaa82a74fcb5a39778bfbe41c5b
ArielDcg <cadenasantisteban@gmail.com>
2024-06-07 20:51:30 -0500

    Avance LogRegisto
    Falta unos ultimos detalles

--- src/front-end/Pages/Home/Home.js
+++ src/front-end/Pages/Home/Home.js
@@ -1,3 +1,31 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.loginLink');
+const registerLink = document.querySelector('.registerLink');
+const btnLogin = document.querySelector('.btnLogin');
+const btnRegister = document.querySelector('.btnRegister');
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+
+});
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+
+});
+
+btnLogin.addEventListener('click', ()=> {
+    wrapper.classList.remove('activeLogin');
+    wrapper.classList.add('activeRegister');
+    
+    });
+
+btnRegister.addEventListener('click', ()=> {    
+    wrapper.classList.remove('activeRegister');
+    wrapper.classList.add('activeLogin');
+
+});
+
 var homeLinks = document.getElementById("homeLinks");
 
     function showMenu(){
